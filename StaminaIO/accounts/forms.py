76ddeceec821c8387f76ca8730279637843f4f84
Lucas Cotrim <lucascotrim.alt@gmail.com>
2024-09-02 16:15:07 -0300

    Revert "mensalidade e pagamento"
    This reverts commit 3304a2b5071fd65af1b6218542b309d9d93c8087.

--- StaminaIO/accounts/forms.py
+++ StaminaIO/accounts/forms.py
@@ -2,6 +2,16 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Usuario
 
 class UsuarioForm(UserCreationForm):
-  class Meta:
-    model = Usuario
-    fields = ['nome', 'email', 'cpf', 'username', 'password1', 'password2']
+    class Meta:
+        model = Usuario
+        fields = ['nome', 'email', 'cpf', 'username', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(UsuarioForm, self).__init__(*args, **kwargs)
+        
+        self.fields['nome'].help_text = None
+        self.fields['email'].help_text = None
+        self.fields['cpf'].help_text = None
+        self.fields['username'].help_text = None
+        self.fields['password1'].help_text = None
+        self.fields['password2'].help_text = None
